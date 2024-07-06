d983a0facd4034e8139100132d8a225af9a675ae
Mikelly Correia <163700231+MiCorreia@users.noreply.github.com>
2024-07-06 15:26:02 -0300

    Componentes das marcas e dos kit criados

--- client/components/indicadorNav/rolagem.tsx
+++ client/components/indicadorNav/rolagem.tsx
@@ -11,3 +11,4 @@ export default function Rolagem(){
         </div>
     )
 }
+
--- client/components/kit/kit.tsx
+++ client/components/kit/kit.tsx
@@ -1,9 +1,28 @@
 import style from "./kit.module.css";
+import Image from "next/image";
+import Kits from "../.././public/kit.svg";
 
 export default function Kit(){
     return(
-        <div className="marcas">
-            <p>KIT</p>
+        <div className={style.kit}>
+            <div className={style.titulo}>
+                <h1>Compre junto</h1>
+            </div>
+
+            <div className={style.elemento}>
+                <div className={style.kits}>
+                    <Image className={style.imagem} src={Kits} alt="" />
+                </div>
+
+                <div className={style.kits}>
+                    <Image className={style.imagem} src={Kits} alt="" />
+                </div>
+
+                <div className={style.kits}>
+                    <Image className={style.imagem} src={Kits} alt="" />
+                </div>
+                
+            </div>
         </div>
     )
 }
