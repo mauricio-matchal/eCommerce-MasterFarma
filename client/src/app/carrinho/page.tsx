30f0303df8cf756209d1412c4f5c89fa72fa3365
Samuel Almeida <samueldealmeidadossantos@gmail.com>
2024-07-09 23:22:06 -0300

    feat: adicao da função de integração para listar os produtos do carrinho na pagina do carrinho, adição do hook useRouter no arquivo cabecalho.tsx e comentarios nessas paginas

--- client/src/app/carrinho/page.tsx
+++ client/src/app/carrinho/page.tsx
@@ -4,8 +4,26 @@ import CarrinhoV from "../../../public/carrinhoVazio.svg";
 import Vistos from "../../../components/ofertas/ofertas";
 import Outros from "../../../components/maisVendidos/vendidos"
 import CarrinhoCheio from "../../../components/carrinhoCheio/carrinhoCheio";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Carrinho(){
+    const [carrinho, setCarrinho] = useState([]);
+
+    useEffect(() => { //função para listar produtos do carrinho
+        const fetchCarrinho = async () => {
+            try {
+                const response = await axios.get('http://localhost:3000/carrinho');
+                setCarrinho(response.data);
+            } catch (error) {
+                console.error('Erro ao buscar produtos do carrinho:', error);
+            }
+        };
+
+        fetchCarrinho();
+    }, []);
+
+    //usar um carrinho.length === 0 para verificar se o carrinho está vazio pode ser uma opção para ver se o carrinho está vazio
     return(
         <>
             <div className={styles.carrinho}>
