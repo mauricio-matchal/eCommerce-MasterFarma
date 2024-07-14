200f6de6d7d06277626bcdc317d41df720c4c6d6
Mauricio Matchal <mauriciomatchal@gmail.com>
2024-07-13 21:05:58 -0300

    modified:   client/src/app/produto/page.tsx

--- client/src/app/produto/page.tsx
+++ client/src/app/produto/page.tsx
@@ -2,6 +2,6 @@ import ProductPage from "@/pages/produto/product-page"
 
 export default function Produto(){
     return(
-        <ProductPage/>
+        <ProductPage title={""} oldPrice={0} price={0} installment={0} code={0}/>
     )
 }
